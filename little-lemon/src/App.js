442f366de63ca60153c59985d063ba72623937db
wuwil1216 <wuwil1216@gmail.com>
2025-10-01 20:20:36 -0500

    Set up basic React template

---
+++ little-lemon/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Homepage
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
